103dd224879c9a4f3a0140a25dcd417684a0a599
Mason <freemason@gmail.com>
2012-08-22 18:47:39 -0700

    yer a wizard harry

--- lib/ruby-conf.rb
+++ lib/ruby-conf.rb
@@ -1,14 +1,29 @@
 #
 #
-# @author Hollin Wilkins & Curtis Schofield
+# @author Hollin Wilkins & Curtis Schofield & Mason Bo-bay-son
+module Magic
+  attr_accessor :chain
+  def gather() "#{to_s}#{chain.nil? ? "" : " #{chain.gather}"}" end
+end
+
 class RubyConf
   @@configs = {}
 
   class Config
-    attr_reader :attributes
+    attr_reader :attributes, :name, :chains, :locked
 
-    def initialize
+    def initialize(name = nil)
+      @locked = false
       @attributes = {}
+      @name = name.to_sym if name
+      @chains = []
+    end
+
+    def lock
+      @locked = true
+      @attributes.each_value do |value|
+        value.lock if value.respond_to? :lock
+      end
     end
 
     def [](name)
@@ -25,9 +40,29 @@ class RubyConf
       @attributes[name.to_sym] = value
     end
 
+    def copy_walk(o)
+      if o.is_a?(Hash)
+        copy = {}
+        o.each do |key, val|
+          copy[key] = copy_walk(val)
+        end
+        copy
+      elsif o.is_a?(Array)
+        copy = []
+        o.each do |i|
+          copy << copy_walk(i)
+        end
+        copy
+      else
+        o
+      end
+    end
+
     def inherit(name, parent)
       self[name] = Config.new
-      self[name].attributes.merge! parent.attributes.clone
+      [parent].flatten.each { |p|
+        self[name.to_sym].attributes.merge! copy_walk(p.attributes)
+      }
     end
 
     def []=(name,value)
@@ -37,11 +72,24 @@ class RubyConf
     def method_missing(name, *args, &block)
       if block_given?
         _inherit(name, args)
-        _set_config_with_block(name,block)
+        _set_config_with_block(name, block)
+        while chain = self[name].chains.pop
+          self[name][chain] = chain.chain.nil? ?  "" : chain.chain.gather
+        end
         return
       end
 
-      super if 0 == args.size && !@attributes.has_key?(name.to_sym)
+      super if @locked && args.size == 0 && !@attributes.has_key?(name.to_sym)
+      if !@attributes.has_key?(name.to_sym) && (args.size == 0 || args.first.is_a?(Magic))
+        str = name.to_s
+        str.extend(Magic)
+        if args.first.is_a? Magic
+          str.chain = args.first
+          @chains.delete_if { |a| a.equal? args.first }
+        end
+        @chains << str
+        return str
+      end
 
       _set_or_get_attribute(name, args)
     end
@@ -56,13 +104,43 @@ class RubyConf
       end
     end
 
+    def build_s(depth = 0)
+      str = ""
+      str += "[#@name]\n" if @name
+      str += "\n"
+      @attributes.keys.map{|k| k.to_s }.sort.each do |key|
+        value = self[key]
+        str += "  " * depth
+        str += "#{key}:"
+        str += value.respond_to?(:build_s) ? value.build_s(depth+1) : " #{value}\n"
+        str += "\n" unless depth > 0
+      end
+      str
+    end
+
+    def to_s
+      build_s
+    end
+
+    def to_str
+      to_s
+    end
+
+    def build_inspect
+     "#{"[#@name] " if @name}#{@attributes.keys.map{|k| k.to_s }.sort.map { |key| "#{key}: #{self[key].respond_to?(:build_s) ? "{ #{self[key].build_inspect} }" : "#{self[key].inspect}"}" }.join(", ")}"
+    end
+
+    def inspect
+      build_inspect
+    end
+
     private
 
     def _set_or_get_attribute(name, args)
       case(args.size)
       when 0
-        # config.something 
-        # => 'value' 
+        # config.something
+        # => 'value'
         self[name]
       when 1
         # config.something "value"
@@ -112,8 +190,9 @@ class RubyConf
   #  @param [Symbol] namespace of the config
   #  @param [Hash] list of options. e.g. :as => ConstantName 
   def self.define(name = nil , options = {}, &block)
-    config = Config.new
+    config = Config.new(name)
     config.instance_eval &block
+    config.lock
 
     @@configs[name.to_sym] = config unless name.nil?
     if options.has_key? :as
--- spec/lib/ruby-conf_spec.rb
+++ spec/lib/ruby-conf_spec.rb
@@ -14,6 +14,90 @@ require 'ruby-conf'
 
 describe RubyConf do
   subject { RubyConf }
+
+  describe ".to_s" do
+    it "prints out the config in a human readable way" do
+      RubyConf.define("some shapes", :as => :Shapes) {
+        defaults { position { px 10; py 20 }; size { width 100; height 200 }; rotation lambda { '90 degrees' } }
+        other { sides 4; color blue like the color of the sea before a storm }
+        triangle(:inherits => defaults) { named :rectangle; size { width 50 }; rotation '180 degrees' }
+        square(:inherits => defaults) { named :rectangle; size { width 50 } }
+        circle(:inherits => [defaults, other]) { sides 0; rotation lambda { 'who could possibly tell?' }; fits { pegs { round lambda { "yes" }; square lambda { "no" } } } }
+        polygon { sides 'many'; details { named 'somename'; actual_sides 100; discussion { seems? 'like a lot of damn sides' } } }
+        dafuq? { holy fuck this is some god damn evil fucking black sorcery!; how the hell did he make this happen?; mason is some kind of sorcerer; this is freaking me out man; srsly dude }
+      }
+
+      Shapes.to_s.should == <<-TEXT
+[some shapes]
+
+circle:
+  color: blue like the color of the sea before a storm
+  fits:
+    pegs:
+      round: yes
+      square: no
+  position:
+    px: 10
+    py: 20
+  rotation: who could possibly tell?
+  sides: 0
+  size:
+    height: 200
+    width: 50
+
+dafuq?:
+  holy: fuck this is some god damn evil fucking black sorcery!
+  how: the hell did he make this happen?
+  mason: is some kind of sorcerer
+  srsly: dude
+  this: is freaking me out man
+
+defaults:
+  position:
+    px: 10
+    py: 20
+  rotation: 90 degrees
+  size:
+    height: 200
+    width: 50
+
+other:
+  color: blue like the color of the sea before a storm
+  sides: 4
+
+polygon:
+  details:
+    actual_sides: 100
+    discussion:
+      seems?: like a lot of damn sides
+    named: somename
+  sides: many
+
+square:
+  named: rectangle
+  position:
+    px: 10
+    py: 20
+  rotation: 90 degrees
+  size:
+    height: 200
+    width: 50
+
+triangle:
+  named: rectangle
+  position:
+    px: 10
+    py: 20
+  rotation: 180 degrees
+  size:
+    height: 200
+    width: 50
+
+TEXT
+      Shapes.inspect.should == '[some shapes] circle: { color: "blue like the color of the sea before a storm", fits: { pegs: { round: "yes", square: "no" } }, position: { px: 10, py: 20 }, rotation: "who could possibly tell?", sides: 0, size: { height: 200, width: 50 } }, dafuq?: { holy: "fuck this is some god damn evil fucking black sorcery!", how: "the hell did he make this happen?", mason: "is some kind of sorcerer", srsly: "dude", this: "is freaking me out man" }, defaults: { position: { px: 10, py: 20 }, rotation: "90 degrees", size: { height: 200, width: 50 } }, other: { color: "blue like the color of the sea before a storm", sides: 4 }, polygon: { details: { actual_sides: 100, discussion: { seems?: "like a lot of damn sides" }, named: "somename" }, sides: "many" }, square: { named: :rectangle, position: { px: 10, py: 20 }, rotation: "90 degrees", size: { height: 200, width: 50 } }, triangle: { named: :rectangle, position: { px: 10, py: 20 }, rotation: "180 degrees", size: { height: 200, width: 50 } }'
+    end
+
+  end
    
   describe ".define" do
     context "no arguments" do
@@ -178,7 +262,7 @@ describe RubyConf do
         subject.cake.should respond_to(:skim_milk)
       end
       it "our cake has no bees and raises NameError" do
-        lambda do 
+        lambda do
           subject.cake.bees
         end.should raise_error NameError
       end

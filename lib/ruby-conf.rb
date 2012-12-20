77fa0470db42bd18f1347855a587a78f7e9fb72b
Mason <freemason@gmail.com>
2012-12-19 17:22:03 -0800

    procs and lambdas now have 'self' set to the parent conf object so they can search the tree for values

--- lib/ruby-conf.rb
+++ lib/ruby-conf.rb
@@ -2,230 +2,151 @@
 #
 # @author Hollin Wilkins & Curtis Schofield & Mason Bo-bay-son
 module Magic
-  attr_accessor :chain
-  def gather() "#{to_s}#{chain.nil? ? "" : " #{chain.gather}"}" end
+  attr_accessor :__rc_chain
+  def __rc_gather() "#{to_s}#{__rc_chain.nil? ? "" : " #{__rc_chain.__rc_gather}"}" end
 end
 
-class RubyConf
-  @@configs = {}
+class RubyConf < Object
+  @@__rc_configs = {}
 
   class Config
-    attr_reader :attributes, :name, :chains, :locked
+    attr_reader :__rc_attributes, :__rc_parent, :__rc_name, :__rc_chains, :__rc_locked
 
-    def initialize(name = nil)
-      @locked = false
-      @attributes = {}
-      @name = name.to_sym if name
-      @chains = []
+    def __rc_root() __rc_parent ? __rc_parent.__rc_root : self end
+
+    def initialize(name = nil, parent = nil, &block)
+      @__rc_locked, @__rc_attributes, @__rc_chains, @__rc_parent = false, {}, [], parent
+      @__rc_name = name.to_sym if name
+      instance_eval(&block) if block_given?
+      __rc_lock
     end
 
-    def lock
-      @locked = true
-      @attributes.each_value do |value|
-        value.lock if value.respond_to? :lock
-      end
+    def __rc_lock
+      @__rc_locked = true
+      @__rc_attributes.each_value { |value| value.__rc_lock if value.is_a?(Config) }
+      self
     end
 
     def [](name, args = nil)
-      value = @attributes[name.to_sym]
-
-      if value.is_a?(Proc)
-        value.call(*args)
-      else
-        value
-      end
+      value = @__rc_attributes[name.to_sym]
+      value.is_a?(Proc) ? __rc_root.instance_exec(*args, &value) : value
     end
 
-    def []=(name,value)
-      @attributes[name.to_sym] = value
-    end
-
-    def copy_walk(o)
+    def __rc_copy(o)
       if o.is_a?(Hash)
-        copy = {}
-        o.each do |key, val|
-          copy[key] = copy_walk(val)
-        end
-        copy
+        o.inject({}) { |copy, (key, val)| copy[key] = __rc_copy(val); copy }
       elsif o.is_a?(Array)
-        copy = []
-        o.each do |i|
-          copy << copy_walk(i)
-        end
-        copy
+        o.inject([]) { |copy, i| copy << __rc_copy(i); copy }
       else
         o
       end
     end
 
-    def inherit(name, parent)
-      self[name] = Config.new
-      [parent].flatten.each { |p|
-        self[name.to_sym].attributes.merge! copy_walk(p.attributes)
-      }
+    def []=(name, value)
+      @__rc_attributes[name.to_sym] = value
     end
 
-    def []=(name,value)
-      @attributes[name.to_sym] = value
+    def __rc_set_default(conf, key, value)
+      if conf.__rc_attributes.key?(key)
+        if value.is_a?(Config)
+          sub = conf.__rc_attributes[key]
+          value.__rc_attributes.each do |k, v|
+            __rc_set_default(sub, k, v)
+          end
+        end
+      else
+        conf.__rc_attributes[key] = value
+      end
     end
 
     def method_missing(name, *args, &block)
+      name = name.to_sym
+      options = args.last.is_a?(Hash) ? args.last : {}
+
       if block_given?
-        _inherit(name, args)
-        _set_config_with_block(name, block)
-        while chain = self[name].chains.pop
-          self[name][chain] = chain.chain.nil? ?  "" : chain.chain.gather
+        if options.key?(:inherits)
+          self[name] = [*options[:inherits]].inject(Config.new(name, self, &block)) do |conf, inherited|
+            inherited = self[inherited.to_sym] unless inherited.is_a?(Config)
+            __rc_copy(inherited.__rc_attributes).each do |key, value|
+              __rc_set_default(conf, key, value)
+            end
+            conf
+          end
+        elsif self[name].is_a?(Config)
+          self[name].instance_eval(&block)
+        else
+          self[name] = Config.new(name, self, &block)
         end
-        return
+
+        while (chain = self[name].__rc_chains.pop)
+          self[name][chain] = chain.__rc_chain.nil? ?  "" : chain.__rc_chain.__rc_gather
+        end
+
+      else
+
+        super if @__rc_locked && args.size == 0 && !@__rc_attributes.key?(name)
+
+        if !@__rc_attributes.key?(name) && (args.size == 0 || args.first.is_a?(Magic))
+          str = name.to_s
+          str.extend(Magic)
+          if args.first.is_a?(Magic)
+            str.__rc_chain = args.first
+            @__rc_chains.delete_if { |a| a.equal? args.first }
+          end
+          @__rc_chains << str
+          return str
+        end
+
+        if args.empty?
+          self[name]
+        else
+          args = args.size == 1 ? args.first : args
+          (@__rc_locked && __rc_attributes[name.to_sym].is_a?(Proc)) ? self[name, args] : self[name] = args
+        end
+
       end
 
-      super if @locked && args.size == 0 && !@attributes.has_key?(name.to_sym)
-      if !@attributes.has_key?(name.to_sym) && (args.size == 0 || args.first.is_a?(Magic))
-        str = name.to_s
-        str.extend(Magic)
-        if args.first.is_a? Magic
-          str.chain = args.first
-          @chains.delete_if { |a| a.equal? args.first }
-        end
-        @chains << str
-        return str
-      end
-
-      _set_or_get_attribute(name, args)
     end
 
     def respond_to?(name)
-      if @attributes.has_key? name.to_sym
-        true
-      elsif @parent
-        @parent.respond_to?(name)
-      else
-        super
-      end
+      super || @__rc_attributes.key?(name) || @__rc_parent.respond_to?(name)
     end
 
-    def build_s(depth = 0)
+    def __rc_build_string(depth = 0)
       str = ""
-      str += "[#@name]\n" if @name
+      str += "[#@__rc_name]\n" unless @__rc_parent
       str += "\n"
-      @attributes.keys.map{|k| k.to_s }.sort.each do |key|
+      @__rc_attributes.keys.map{|k| k.to_s }.sort.each do |key|
         value = self[key]
         str += "  " * depth
         str += "#{key}:"
-        str += value.respond_to?(:build_s) ? value.build_s(depth+1) : " #{value}\n"
+        str += value.is_a?(Config) ? value.__rc_build_string(depth+1) : " #{value}\n"
         str += "\n" unless depth > 0
       end
       str
     end
+    def to_s() __rc_build_string end
+    def to_str() to_s end
 
-    def to_s
-      build_s
-    end
-
-    def to_str
-      to_s
-    end
-
-    def build_inspect
-     "#{"[#@name] " if @name}#{@attributes.keys.map{|k| k.to_s }.sort.map { |key| "#{key}: #{self[key].respond_to?(:build_s) ? "{ #{self[key].build_inspect} }" : "#{self[key].inspect}"}" }.join(", ")}"
-    end
-
-    def inspect
-      build_inspect
-    end
-
-    private
-
-    def _set_or_get_attribute(name, args)
-      case(args.size)
-      when 0
-        # config.something
-        # => 'value'
-        self[name]
-      when 1
-        # config.something "value"
-        if @locked && attributes[name.to_sym].is_a?(Proc)
-          self[name, args.first]
-        else
-          self[name] = args.first
-        end
-      else
-        # config.something "value", "value2"
-        if @locked && attributes[name.to_sym].is_a?(Proc)
-          self[name, args]
-        else
-          self[name] = args
-        end
-      end
-    end
-
-    def _update_config_with_block(name,block)
-      self[name].instance_eval(&block)
-    end
-
-    def _new_config_with_block(name, block)
-      self[name] = RubyConf.define(&block)
-    end
-
-    def _set_config_with_block(name, block)
-      if self[name].is_a?(Config)
-        _update_config_with_block(name,block)
-      else
-        _new_config_with_block(name,block)
-      end
-    end
-
-    def _inherit(name, args)
-      if args.size > 0 && args.first.is_a?(Hash) && args.first.has_key?(:inherits)
-        inherit name, args.first[:inherits]
-      end
-    end
+    def __rc_build_inspect() "#{"[#@__rc_name] " unless @__rc_parent}#{@__rc_attributes.keys.map {|k| k.to_s }.sort.map { |key| "#{key}: #{self[key].is_a?(Config) ? "{ #{self[key].__rc_build_inspect} }" : self[key].inspect}" }.join(", ")}" end
+    def inspect() __rc_build_inspect end
   end
-  #  Define a configuration:
-  #
-  #  RubyConf.define "monkey" , :as => 'MyBonobo' do
-  #   has_legs true
-  #   number_arms 2
-  #   number_legs 2
-  #   name 'Nancy Drew'
-  #   age 34
-  #   number_of_bananas_eaten lambda { 
-  #     BanannaChomper.lookup("nancy.bananas").count
-  #   }
-  #  end
-  #
-  #
-  #  @param [Symbol] namespace of the config
-  #  @param [Hash] list of options. e.g. :as => ConstantName 
-  def self.define(name = nil , options = {}, &block)
-    config = Config.new(name)
-    config.instance_eval &block
-    config.lock
 
-    @@configs[name.to_sym] = config unless name.nil?
-    if options.has_key? :as
-      Object.const_set(options[:as].to_s.to_sym, config)
+  def self.define(name = nil, options = {}, &block)
+    config = Config.new(name, &block)
+    @@__rc_configs[name.to_sym] = config unless name.nil?
+
+    const = options.fetch(:as, name)
+    if const && const.to_s[/^[A-Z]/]
+      const = const.to_sym
+      Object.const_set(const, config) if !Object.const_defined?(const) || Object.const_get(const).is_a?(Config)
     end
     config
   end
 
-  def self.[](name)
-    @@configs[name.to_sym]
-  end
+  def self.[](name) @@__rc_configs[name.to_sym] end
 
-  def self.method_missing(name, *args, &block)
-    if @@configs.has_key? name.to_sym
-      @@configs[name.to_sym]
-    else
-      super
-    end
-  end
+  def self.method_missing(name, *args) @@__rc_configs[name.to_sym] end
 
-  def self.respond_to?(name)
-    if @@configs.has_key? name.to_sym
-      true
-    else
-      super
-    end
-  end
+  def self.respond_to?(name) @@__rc_configs.key?(name.to_sym) end
 end
--- spec/lib/ruby-conf_spec.rb
+++ spec/lib/ruby-conf_spec.rb
@@ -1,17 +1,6 @@
 require 'spec_helper'
 require 'ruby-conf'
 
-# write it
-#
-#RubyConf.define "namespace" do
-# thingy 'aoeuaoe'
-#end
-#
-## read it
-#
-#RubyConf.namespace.thingy 
-
-
 describe RubyConf do
   subject { RubyConf }
 
@@ -33,6 +22,55 @@ describe RubyConf do
 
       Argue.you_are_a('My Love?').should == "Why are you such a jerk, My Love?"
     end
+  end
+
+  describe ".root" do
+
+    it "has a heirarchy" do
+      RubyConf.define "RootTest" do
+        outer do
+          middle do
+            inner do
+              val value
+            end
+          end
+        end
+      end
+
+      RootTest.outer.middle.inner.val.should == "value"
+
+      RootTest.__rc_parent.should be_nil
+      RootTest.outer.__rc_parent.should == RootTest
+      RootTest.outer.middle.__rc_parent.should == RootTest.outer
+      RootTest.outer.middle.inner.__rc_parent.should == RootTest.outer.middle
+
+      RootTest.__rc_root.should == RootTest
+      RootTest.outer.__rc_root.should == RootTest
+      RootTest.outer.middle.__rc_root == RootTest
+      RootTest.outer.middle.inner.__rc_root.should == RootTest
+
+    end
+
+    it "sets self properly in nested procs" do
+      RubyConf.define "SelfSetTest" do
+        val proc { self }
+        outer do
+          val proc { self }
+          middle do
+            val proc { self }
+            inner do
+              val proc { self }
+            end
+          end
+        end
+      end
+
+      SelfSetTest.val.should == SelfSetTest
+      SelfSetTest.outer.val.should == SelfSetTest
+      SelfSetTest.outer.middle.val.should == SelfSetTest
+      SelfSetTest.outer.middle.inner.val.should == SelfSetTest
+
+    end
 
   end
 
@@ -41,14 +79,14 @@ describe RubyConf do
       RubyConf.define("some shapes", :as => :Shapes) {
         defaults { position { px 10; py 20 }; size { width 100; height 200 }; rotation lambda { '90 degrees' } }
         other { sides 4; color blue like the color of the sea before a storm }
-        triangle(:inherits => defaults) { named :rectangle; size { width 50 }; rotation '180 degrees' }
+        triangle(:inherits => 'defaults') { named :rectangle; size { width 5 }; rotation '180 degrees' }
         square(:inherits => defaults) { named :rectangle; size { width 50 } }
-        circle(:inherits => [defaults, other]) { sides 0; rotation lambda { 'who could possibly tell?' }; fits { pegs { round lambda { "yes" }; square lambda { "no" } } } }
+        circle(:inherits => [:defaults, :other]) { sides 0; rotation lambda { 'who could possibly tell?' }; fits { pegs { round lambda { "yes" }; square lambda { "no" } } } }
         polygon { sides 'many'; details { named 'somename'; actual_sides 100; discussion { seems? 'like a lot of damn sides' } } }
         dafuq? { holy fuck this is some god damn evil fucking black sorcery!; how the hell did he make this happen?; mason is some kind of sorcerer; this is freaking me out man; srsly dude }
       }
 
-      Shapes.inspect.should == '[some shapes] circle: { color: "blue like the color of the sea before a storm", fits: { pegs: { round: "yes", square: "no" } }, position: { px: 10, py: 20 }, rotation: "who could possibly tell?", sides: 0, size: { height: 200, width: 50 } }, dafuq?: { holy: "fuck this is some god damn evil fucking black sorcery!", how: "the hell did he make this happen?", mason: "is some kind of sorcerer", srsly: "dude", this: "is freaking me out man" }, defaults: { position: { px: 10, py: 20 }, rotation: "90 degrees", size: { height: 200, width: 50 } }, other: { color: "blue like the color of the sea before a storm", sides: 4 }, polygon: { details: { actual_sides: 100, discussion: { seems?: "like a lot of damn sides" }, named: "somename" }, sides: "many" }, square: { named: :rectangle, position: { px: 10, py: 20 }, rotation: "90 degrees", size: { height: 200, width: 50 } }, triangle: { named: :rectangle, position: { px: 10, py: 20 }, rotation: "180 degrees", size: { height: 200, width: 50 } }'
+      Shapes.inspect.should == '[some shapes] circle: { color: "blue like the color of the sea before a storm", fits: { pegs: { round: "yes", square: "no" } }, position: { px: 10, py: 20 }, rotation: "who could possibly tell?", sides: 0, size: { height: 200, width: 100 } }, dafuq?: { holy: "fuck this is some god damn evil fucking black sorcery!", how: "the hell did he make this happen?", mason: "is some kind of sorcerer", srsly: "dude", this: "is freaking me out man" }, defaults: { position: { px: 10, py: 20 }, rotation: "90 degrees", size: { height: 200, width: 100 } }, other: { color: "blue like the color of the sea before a storm", sides: 4 }, polygon: { details: { actual_sides: 100, discussion: { seems?: "like a lot of damn sides" }, named: "somename" }, sides: "many" }, square: { named: :rectangle, position: { px: 10, py: 20 }, rotation: "90 degrees", size: { height: 200, width: 50 } }, triangle: { named: :rectangle, position: { px: 10, py: 20 }, rotation: "180 degrees", size: { height: 200, width: 5 } }'
       Shapes.to_s.should == <<-TEXT
 [some shapes]
 
@@ -65,7 +103,7 @@ circle:
   sides: 0
   size:
     height: 200
-    width: 50
+    width: 100
 
 dafuq?:
   holy: fuck this is some god damn evil fucking black sorcery!
@@ -81,7 +119,7 @@ defaults:
   rotation: 90 degrees
   size:
     height: 200
-    width: 50
+    width: 100
 
 other:
   color: blue like the color of the sea before a storm
@@ -113,7 +151,7 @@ triangle:
   rotation: 180 degrees
   size:
     height: 200
-    width: 50
+    width: 5
 
 TEXT
     end
@@ -159,6 +197,7 @@ TEXT
         inherited_config.city.thing.origin.should == "ocean"
       end
     end
+
     context ":as" do 
       it "creates a global Constant" do
         subject.define "rails_database", :as => :RailsDatabase do
@@ -169,6 +208,7 @@ TEXT
         ::RailsDatabase.production[:password].should_not be_nil
       end
     end
+
     it "can reopen configs" do
       config = subject.define do
         godzilla do
@@ -182,6 +222,7 @@ TEXT
       config.godzilla.spines.should == true
       config.godzilla.awesome.should == true
     end
+
     it "returns a named config" do
       config = subject.define "a_name" do
         something "hi"
@@ -189,6 +230,7 @@ TEXT
       config.something.should == "hi"
       subject.a_name.something.should == 'hi'
     end
+
     it "can be chained" do
       subject.define "config", :as => :MyConfig do
         love_song (RubyConf.define "love_song" do
@@ -197,6 +239,7 @@ TEXT
       end
       MyConfig.love_song.title.should == 'in me all along'
     end
+
     it "can be chained using do blocks instead of RubyConf.define" do
       subject.define "config", :as => :MyConfig do
         turtles do
@@ -212,6 +255,7 @@ TEXT
       MyConfig.turtles.mutant.should == true
       MyConfig.turtles.raphael.mask.should == "red"
     end
+
     describe "RubyConf" do
       it "can access configs using square brackets" do
         subject.define "config" do
